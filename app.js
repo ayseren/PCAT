c7f2ec0ff3bf99bad4b508d4047422618edc4f9e
ayseren <ayse.eren@windowslive.com>
2021-10-27 16:24:43 +0300

    express and nodemon added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
- 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('asdfadf');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`${port}`);
+});
