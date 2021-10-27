fc58ef30e98609c647cd90f26fe7a21b4d6dbc41
ayseren <ayse.eren@windowslive.com>
2021-10-27 19:27:03 +0300

    middleware example

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+//middleware
+// const myLogger = (req, res, next) => {
+//     console.log("ayse");
+//     next();
+// }
+
+app.use(express.static('public'))
+//app.use(myLogger);
+
 app.get('/', (req, res) => {
     res.send('asdfadf');
 });
