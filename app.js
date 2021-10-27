906e2e02e42307f6b44986b4c2f204baea74ad5e
ayseren <ayse.eren@windowslive.com>
2021-10-27 20:31:48 +0300

    express.static middleware used

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 //middleware
-// const myLogger = (req, res, next) => {
-//     console.log("ayse");
-//     next();
-// }
+const myLogger = (req, res, next) => {
+    console.log("ayse");
+    next(); //bir sonraki middleware ilerlemek icin
+}
 
 app.use(express.static('public'))
-//app.use(myLogger);
+app.use(myLogger);
 
 app.get('/', (req, res) => {
-    res.send('asdfadf');
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
